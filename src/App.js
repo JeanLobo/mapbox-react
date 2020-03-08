038eecc0c6817196f0189aab545a0ff27df9c81a
Jean Lobo <jeanloboxd@gmail.com>
2020-03-08 18:54:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Map from "./components/Map";
+import SearchableMap from "./components/SearchableMap";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Map} />
+          <Route exact path="/search" component={SearchableMap} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
